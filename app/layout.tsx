69f60e6f0bbf929b790492c4ecc5be9d2c7b3ef5
wsnustrw <wsn.strw@gmail.com>
2025-07-14 16:01:03 +0700

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import './globals.css';
 import { SessionProvider } from 'next-auth/react';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  metadataBase: new URL('https://ai.wsnu.dev'),
+  title: 'SeeuAI - WSNU DEV',
+  description: ' AI Chatbot WNSU DEV build with AI SDK.',
 };
 
 export const viewport = {
